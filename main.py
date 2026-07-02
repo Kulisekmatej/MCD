2d4e543dcfd82b46b9a04cca7922403162616066
Matěj Kulíšek <matej.kulisek99@icloud.com>
2026-07-02 12:10:18 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Spouštěč aplikace – otevře grafické okno.
+
+Spuštění z příkazové řádky:  python main.py
+Po zabalení do .exe stačí na soubor dvakrát kliknout.
+"""
+
+from hours_tracker.gui import main
+
+if __name__ == "__main__":
+    main()
